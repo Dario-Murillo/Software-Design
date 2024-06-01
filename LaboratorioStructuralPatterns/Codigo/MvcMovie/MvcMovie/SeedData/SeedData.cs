2a9729306506161b171d09ee9409fdf97c1d4ff2
dario.murillo <dario.murillo@ucr.ac.cr>
2024-06-01 16:21:07 -0600

    finish MVC

--- LaboratorioStructuralPatterns/Codigo/MvcMovie/MvcMovie/SeedData/SeedData.cs
+++ LaboratorioStructuralPatterns/Codigo/MvcMovie/MvcMovie/SeedData/SeedData.cs
@@ -15,17 +15,14 @@ public static class SeedData
             serviceProvider.GetRequiredService<
                 DbContextOptions<MvcMovieContext>>()))
         {
-            // Look for any movies.
-            if (context.Movie.Any())
-            {
-                return;   // DB has been seeded
-            }
+
             context.Movie.AddRange(
                 new Movie
                 {
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating = "A",
                     Price = 7.99M
                 },
                 new Movie
@@ -33,6 +30,7 @@ public static class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "A",
                     Price = 8.99M
                 },
                 new Movie
@@ -40,6 +38,7 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "A",
                     Price = 9.99M
                 },
                 new Movie
@@ -47,6 +46,7 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "13",
                     Price = 3.99M
                 }
             );
