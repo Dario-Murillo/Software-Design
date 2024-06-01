462c66115b58247f93ab8d5a86c22dcc4786587d
Fabian <fabiguzmang1205@gmail.com>
2024-06-01 12:12:07 -0600

    Search

--- LaboratorioStructuralPatterns/Codigo/MvcMovie/MvcMovie/Models/Movie.cs
+++ LaboratorioStructuralPatterns/Codigo/MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,4 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
+
+//namespace MvcMovie.Models;
+
+//public class Movie
+//{
+//    public int Id { get; set; }
+//    public string? Title { get; set; }
+//    [DataType(DataType.Date)]
+//    public DateTime ReleaseDate { get; set; }
+//    public string? Genre { get; set; }
+//    public decimal Price { get; set; }
+//}
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -6,8 +22,11 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
